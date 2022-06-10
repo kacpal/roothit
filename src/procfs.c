e83b29b9228cff416d957704dcd9311b8940ddbb
kacpal <kacper.paluch6@gmail.com>
2022-06-10 15:43:33 +0200

    procfs bug fix

--- src/procfs.c
+++ src/procfs.c
@@ -5,27 +5,29 @@
 
 static struct proc_dir_entry *proc_ent;
 
-static ssize_t p_write(struct file *file, const char __user *ubuf, size_t count, loff_t *ppos) {
+static ssize_t p_read(struct file *file, char __user *ubuf, size_t count, loff_t *ppos) {
 	return -1;
 }
 
-static ssize_t p_read(struct file *file, char __user *ubuf, size_t count, loff_t *ppos) {
+static ssize_t p_write(struct file *file, const char __user *ubuf, size_t count, loff_t *ppos) {
 	char buf[BUFFER_SIZE];
+      int i;
 
-      if (copy_from_user(ubuf, buf, 1))
+      if (copy_from_user(buf, ubuf, 1))
             return -EFAULT;
 
-      if (buf[0] == 0) {
+	sscanf(buf, "%d", &i);
+      if (i == 0) {
             sched_exit();
-            printk(KERN_DEBUG "roothit: Scheduled tasks switched off\n");
+            printk(KERN_INFO "roothit: Scheduled tasks switched off\n");
       }
-      if (buf[0] == 1) {
+      if (i == 1) {
             sched_init();
-            printk(KERN_DEBUG "roothit: Scheduled tasks switched on\n");
+            printk(KERN_INFO "roothit: Scheduled tasks switched on\n");
       }
-      if (buf[0] == 2) {
+      if (i == 2) {
             run_checks();
-            printk(KERN_DEBUG "roothit: Running all checks\n");
+            printk(KERN_INFO "roothit: Running all checks\n");
       }
 
 	return count;
@@ -37,7 +39,7 @@ struct proc_ops fops = {
 };
 
 void init_procfs(void) {
-      proc_ent = proc_create("roothit", 0644, NULL, &fops);
+      proc_ent = proc_create("roothit", 0666, NULL, &fops);
 }
 
 void cleanup_procfs(void) {
